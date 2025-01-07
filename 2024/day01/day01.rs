e63daae96eaf29bff7783f46507b762b88f779c1
a-stephen <sadjignon015@gmail.com>
2025-01-07 16:40:35 +0000

    Day 01 part B; **

--- 2024/day01/day01.rs
+++ 2024/day01/day01.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::HashMap;
 
 fn parse_input(input: &str) -> (Vec<i32>, Vec<i32>) {
     let mut left: Vec<i32> = vec![];
@@ -26,20 +27,26 @@ fn compute_difference(mut left: Vec<i32>, mut right: Vec<i32>) -> i32 {
     sum
 }
 
+fn compute_list_similarity(left: Vec<i32>, right: Vec<i32>) -> i32 {
+    let mut right_map: HashMap<i32, i32> = HashMap::new();
+
+    for ritem in right {
+        *right_map.entry(ritem).or_insert(0) += 1;
+    }
+    
+    let iter_sum: i32 = left.iter().map(
+        |x| x * right_map.get(x).unwrap_or(&0)
+    ).sum();
+
+    iter_sum
+}
+
 fn main() {
     let file_path = "./data/r_input.txt";
     let input = fs::read_to_string(file_path).unwrap();
 
     let (left, right) = parse_input(&input);
+    let right_occurence = compute_list_similarity(left.clone(), right.clone());
 
-    let sum = compute_difference(left.clone(), right.clone());
-    
-    println!("{:?}", sum);
-    // println!("{:?}", parsed_input)
-    // let (mut left: Vec<i64>, mut right: Vec<i64>) = input.lines().map(
-    //     |x: &str| {
-    //         let pairs: Vec<&str> = x.split("   ").collect::<Vec<&str>>();
-    //         (pairs[0].parse::<i64>().unwrap(), pairs[1].parse::<i64>().unwrap())
-    //     }
-    // ).collect::<{Vec<i64>, Vec<i64>}>();
+    println!("{:?}", right_occurence);
 }
